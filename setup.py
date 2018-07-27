5976520b1bd075a838680654169d7821fdb7a9cd
Alisson Lauffer <alissonvitortc@gmail.com>
2018-07-27 19:17:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0',
     packages=[''],
     url='https://github.com/marminino/text2ml',
-    author='Alisson',
+    author='Alisson Lauffer',
     author_email='alissonvitortc@gmail.com',
     description='A module for Telegram Bot API that can format text + entities into formatted text'
 )
