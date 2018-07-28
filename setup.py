bf40140a9c0d1c63eb1a6e18762a29a2c6acb25e
Alisson Lauffer <alissonvitortc@gmail.com>
2018-07-28 00:51:07 -0300

    Version 2.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='text2ml',
-    version='2.0',
+    version='2.0.1',
     packages=[''],
     url='https://github.com/marminino/text2ml',
     author='Alisson Lauffer',
